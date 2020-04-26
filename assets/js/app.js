bc9a4fce22e49c99d203864a4d93df3b8799fd6d
Pato Montecchiarini <53584240+pmontecchiarini@users.noreply.github.com>
2020-04-25 23:50:46 -0300

    Save list items in LocalStorage
    Added the save and delete in local storage functionallity

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,12 @@ const listItems = document.getElementById('list');
 eventListeners();
     
 function eventListeners() {
+    //Submit text area
     document.querySelector('#form').addEventListener('submit', addItem);
-
+    //Delete list item
     list.addEventListener('click', deleteItem);
+    //Load content
+    document.addEventListener('DOMContentLoaded', localStorageGet);
 }
 
 
@@ -27,12 +30,71 @@ function addItem (e){
     li.innerText = text;
     li.appendChild(deleteButton);
     list.appendChild(li);
-    document.getElementById('text').value = ''   
+    document.getElementById('text').value = ''
+
+    //Add to local storage
+    addToLocalStorage(text)   
 }
     //Deleting list items
 function deleteItem (e){
     e.preventDefault();
     if (e.target.className === 'delete-item'){
-        console.log(e.target.parentElement.remove());
+        e.target.parentElement.remove();
+        deleteTextFromLocalStorage(e.target.parentElement.innerText);
     } 
+}
+
+//Load Local Storage saved items
+
+function localStorageGet(){
+    let items;
+    items = getItemsFromLocalStorage ();
+    items.forEach( text =>{
+        const deleteButton = document.createElement('a');
+        deleteButton.classList = 'delete-item';
+        deleteButton.innerText = 'X';
+    
+        //Adding contento to list items
+        const li = document.createElement('li');
+        li.innerText = text;
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+    })
+}
+
+//Add items to local storage
+function addToLocalStorage(text){
+    let items;
+    items =  getItemsFromLocalStorage ();
+    items.push(text);
+    //Saves items as an array
+    localStorage.setItem('items', JSON.stringify(items))
+ 
+}
+
+//Checks hat items are in local storage
+function getItemsFromLocalStorage () {
+    let items; 
+    if (localStorage.getItem('items') === null ){
+        items = [];
+    }else {
+        items= JSON.parse(localStorage.getItem('items'));
+    }
+    return items
+
+}
+
+//Delet List Item From Local Storage
+function deleteTextFromLocalStorage(text){
+    let items, textDelete;
+    //Delete X from Item
+    textDelete = text.substring(0, text.length - 1);
+
+    items = getItemsFromLocalStorage();
+    items.forEach(function(text, index){
+        if(textDelete === text){
+            items.splice(index, 1);
+        }
+    });
+    localStorage.setItem('items', JSON.stringify(items));
 }
